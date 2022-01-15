bd873cf9d86bc79a1e78be2becbdc5a2140b4b8a
José Lucas da Silva <joselucas0303@gmail.com>
2022-01-15 17:04:08 -0300

    chore: added google fonts dependency

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'pages/home_page.dart';
 
 void main() {
@@ -13,8 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Pharma Inc',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+          primarySwatch: Colors.blue, textTheme: GoogleFonts.nunitoTextTheme()),
       home: const HomePage(),
     );
   }
