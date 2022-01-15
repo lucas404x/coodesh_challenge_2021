8657039d2836ef713bf5802f4198e3a124466b98
José Lucas da Silva <joselucas0303@gmail.com>
2022-01-15 18:42:37 -0300

    feat: add app colors

---
+++ lib/constants/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Colors {
+  static const Color blue = Color(0xFF0083CA);
+  static const Color aqua = Color(0xFF00AFAD);
+  static const Color yellow = Color(0xFFFCAF17);
+  static const Color orange = Color(0xFFF26522);
+  static const Color deepBlue = Color(0xFF003461);
+}
