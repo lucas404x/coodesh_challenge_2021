24c534f29109362d5f0ebe2a9a02c83433320e12
José Lucas da Silva <joselucas0303@gmail.com>
2022-01-19 22:36:47 -0300

    feat: add bloc support

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
+import 'package:http/http.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'data/repositories/patient_repository.dart';
 import 'pages/home_page.dart';
 
 void main() {
@@ -11,11 +14,14 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Pharma Inc',
-      theme: ThemeData(
-          primarySwatch: Colors.blue, textTheme: GoogleFonts.nunitoTextTheme()),
-      home: const HomePage(),
-    );
+    return RepositoryProvider(
+        create: (context) => PatientRepository(Client()),
+        child: MaterialApp(
+          title: 'Pharma Inc',
+          theme: ThemeData(
+              primarySwatch: Colors.blue,
+              textTheme: GoogleFonts.nunitoTextTheme()),
+          home: const HomePage(),
+        ));
   }
 }
