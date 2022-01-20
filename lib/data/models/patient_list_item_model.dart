ed42fc146989525fcf6dd7cfd3a05fa01a7a5717
José Lucas da Silva <joselucas0303@gmail.com>
2022-01-19 23:30:21 -0300

    feat: add model to represent patient in a list

---
+++ lib/data/models/patient_list_item_model.dart
@@ -0,0 +1,30 @@
+import '../../utils/date_time_utils.dart';
+import 'patient_model.dart';
+
+class PatientListItemModel {
+  final String id;
+  final String gender;
+  final String profile;
+  final String fullname;
+  final String nationality;
+  final String dateBirthday;
+
+  PatientListItemModel({
+    required this.id,
+    required this.gender,
+    required this.profile,
+    required this.fullname,
+    required this.nationality,
+    required this.dateBirthday,
+  });
+
+  factory PatientListItemModel.fromPatientModel(PatientModel patient) =>
+      PatientListItemModel(
+          id: patient.id,
+          gender: patient.gender,
+          profile: patient.photo,
+          fullname: patient.fullname,
+          nationality: patient.nationality,
+          dateBirthday:
+              DateTimeUtils.formatDateToLocalString(patient.dateBirthday));
+}
